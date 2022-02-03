95e7367b2838a300d720fa17c36c7d46d1356ed7
Cian Butler <ciandevelops@gmail.com>
2022-02-03 18:43:48 -0500

    header complete

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React from "react";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
-import './Header.css';
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase The New Flavour" />
+      <h1 className="app__header-h1">The Key To Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
+        perferendis eveniet esse aspernatur, a quasi qui mollitia voluptates,
+        debitis libero porro ducimus exercitationem saepe tempore provident eum
+        vitae alias beatae.
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
+    </div>
+
+    <div className="app__wrapper_img"></div>
   </div>
 );
 
